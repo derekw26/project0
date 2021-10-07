7ecaedef0e2747a7b8f36cfa51e8ba568c1238bb
Derek Wei <derek.wei94@gmail.com>
2021-10-07 18:32:41 +1100

    ver 0.1 added user image upload, tweaked css and html

---
+++ README.md
@@ -0,0 +1,11 @@
+# Tic-tac-toe
+
+Welcome to the wonderful game of Tic Tac Toe created
+
+Technologies used:
+
+- jquery
+-
+
+
+Initially when creating the code, I wanted to use images to represent the X and O on the board
--- js/script.js
+++ js/script.js
@@ -46,11 +46,16 @@ const displayO = function($cell) {
 let winCounterX = 0;
 let winCounterO = 0;
 
+const hStrike = [$('#h1'), $('#h2'), $('#h3')];
+const vStrike = [$('#v1'), $('#v2'), $('#v3')];
+const dStrike = [$('#d1'), $('#d2')];
+
 const winCheckX = function() {
 
   // horizontal
   for (let i = 0; i < gameBoard.length; i++) {
     if ( gameBoard[i][0].hasClass('Xclicked') && gameBoard[i][1].hasClass('Xclicked') && gameBoard[i][2].hasClass('Xclicked')) {
+      hStrike[i].addClass('active Xwin');
       winCounterX++;
       return true;
     }
@@ -58,17 +63,20 @@ const winCheckX = function() {
   // vertical
   for (let i = 0; i < gameBoard.length; i++) {
     if ( gameBoard[0][i].hasClass('Xclicked') && gameBoard[1][i].hasClass('Xclicked') && gameBoard[2][i].hasClass('Xclicked')) {
+      vStrike[i].addClass('active Xwin');
       winCounterX++;
       return true;
     }
   }
   // diagonals
   if ( gameBoard[0][0].hasClass('Xclicked') && gameBoard[1][1].hasClass('Xclicked') && gameBoard[2][2].hasClass('Xclicked')) {
+    dStrike[0].addClass('active Xwin');
     winCounterX++;
     return true;
 
   }
   if ( gameBoard[0][2].hasClass('Xclicked') && gameBoard[1][1].hasClass('Xclicked') && gameBoard[2][0].hasClass('Xclicked')) {
+    dStrike[1].addClass('active Xwin');
     winCounterX++;
     return true;
   }
@@ -80,6 +88,7 @@ const winCheckO = function() {
   // horizontal
   for (let i = 0; i < gameBoard.length; i++) {
     if ( gameBoard[i][0].hasClass('Oclicked') && gameBoard[i][1].hasClass('Oclicked') && gameBoard[i][2].hasClass('Oclicked')) {
+      hStrike[i].addClass('active Owin');
       winCounterO++;
       return true;
     }
@@ -87,16 +96,19 @@ const winCheckO = function() {
   // vertical
   for (let i = 0; i < gameBoard.length; i++) {
     if ( gameBoard[0][i].hasClass('Oclicked') && gameBoard[1][i].hasClass('Oclicked') && gameBoard[2][i].hasClass('Oclicked')) {
+      vStrike[i].addClass('active Owin');
       winCounterO++;
       return true;
     }
   }
   // diagonals
   if ( gameBoard[0][0].hasClass('Oclicked') && gameBoard[1][1].hasClass('Oclicked') && gameBoard[2][2].hasClass('Oclicked')) {
+    dStrike[0].addClass('active Owin');
     winCounterO++;
     return true;
   }
   if ( gameBoard[0][2].hasClass('Oclicked') && gameBoard[1][1].hasClass('Oclicked') && gameBoard[2][0].hasClass('Oclicked')) {
+    dStrike[1].addClass('active Owin');
     winCounterO++;
     return true;
   }
@@ -129,8 +141,11 @@ $('#reset-button').on('click', function() {
   $('#O-button').removeClass('active').prop('disabled', false);
   $('#X-button').removeClass('active').prop('disabled', false);
   $('.cell').removeClass('Xclicked Oclicked')
-  $('#info').html('Select who goes first:');
+  $('#info').html('Select X or O to go first:');
   $('#reset-button').removeClass('active');
+  $('.hstrike').removeClass('active Xwin Owin');
+  $('.vstrike').removeClass('active Xwin Owin');
+  $('.dstrike').removeClass('active Xwin Owin');
 });
 
 // board click functions
@@ -184,7 +199,7 @@ for (let i = 0; i < gameBoard.length; i++) {
         // check for draw after grid is full
         if ($('.cell').length === $('.Xclicked').length + $('.Oclicked').length) {
           if (drawCheck()) {
-            $('#info').html(`DRAW`);
+            $('#info').html(`It's a draw!`);
             $("#O-button").removeClass('active');
             $("#X-button").removeClass('active');
             $('#reset-button').addClass('active');
@@ -198,18 +213,36 @@ for (let i = 0; i < gameBoard.length; i++) {
   }
 }
 
+// upload user tokens for X and O
+
+$('#X-upload').on('change', function () {
+    const fileData = $('#X-upload').prop('files')[0];
+    let reader = new FileReader();
+    reader.readAsDataURL(fileData);
+    reader.onload = function () {
+      $('.x-image').attr('src', reader.result);
+    }
+    reader.onerror = function (error) {
+      prompt('Error: ', error);
+    }
+});
+
+$('#O-upload').on('change', function () {
+    const fileData = $('#O-upload').prop('files')[0];
+    let reader = new FileReader();
+    reader.readAsDataURL(fileData);
+    reader.onload = function () {
+      $('.o-image').attr('src', reader.result);
+    }
+    reader.onerror = function (error) {
+      prompt('Error: ', error);
+    }
+});
+
 // });
 
 // HIGH PRIORITY
-// display visual line through win path
 // scale game for different screen sizes
-
-// LOW PRIORITY
-// add game round indicator
-// direct user to reset button
 // add LocalStorage functionality
-// add half opacity for X and O when cursor hovers over grid cell
-// alter visual button display according to X or O win or draw
-// add fade out transition for X and O button when turn switches
+// add half opacity for X and O when cursor hovers over grid cells
 // change font type
-// can replace .hasClass('classname') with .is('.classname')
